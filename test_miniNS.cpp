e10159d0e17a394666b156e6f0350cfb3328fcd1
Carlos E. Munoz <32044135+munozcar@users.noreply.github.com>
2019-06-24 12:31:32 -0400

    Add files via upload

--- test_miniNS.cpp
+++ test_miniNS.cpp
@@ -10,12 +10,12 @@
 #include <stdlib.h>
 #include "miniNS.h"
 
-// -------------- "LINEAR" FORWARD MODEL ----------------------------------------------------------
-class LinearModel : public Model
+// -------------- "Parabolic" FORWARD MODEL ----------------------------------------------------------
+class ParabolicModel : public Model
 {
     public:
-        LinearModel(const RefArrayXd covariates);
-        ~LinearModel();
+        ParabolicModel(const RefArrayXd covariates);
+        ~ParabolicModel();
         ArrayXd getCovariates();
         virtual void predict(RefArrayXd predictions, const RefArrayXd modelParameters);
 
@@ -23,24 +23,25 @@ class LinearModel : public Model
     private:
 };
 
-LinearModel::LinearModel(const RefArrayXd covariates)
+ParabolicModel::ParabolicModel(const RefArrayXd covariates)
 : Model(covariates)
 {
 }
-LinearModel::~LinearModel()
+ParabolicModel::~ParabolicModel()
 {
 }
 
-void LinearModel::predict(RefArrayXd predictions, RefArrayXd const modelParameters)
+void ParabolicModel::predict(RefArrayXd predictions, RefArrayXd const modelParameters)
 {
-    double slope = modelParameters(0);
-    double offset = modelParameters(1);
-    predictions = slope*covariates + offset;
+    double a = modelParameters(0);
+    double b = modelParameters(1);
+    double c = modelParameters(2);
+    double d = modelParameters(3);
+    predictions = a*exp(-b*covariates) + c*covariates*log(d*covariates);
 }
 
 
 // -------------- "PSG RETRIEVAL" FORWARD MODEL ---------------------------------------------------
-
 std::string exec(const char* cmd) {
     char buffer[128];
     std::string result = "";
@@ -57,7 +58,6 @@ std::string exec(const char* cmd) {
     pclose(pipe);
     return result;
 }
-
 class PSGModel : public Model
 {
     public:
@@ -69,7 +69,6 @@ class PSGModel : public Model
     protected:
     private:
 };
-
 void PSGModel::predict(RefArrayXd predictions, RefArrayXd const modelParameters)
 {
     std::string out = "";
@@ -94,8 +93,8 @@ int main(int argc, char *argv[])
     int Ncols;
     ArrayXXd data;
     string baseInputDirName = "";
-    string inputFileName = "input_data.txt"; // data
-    string outputPathPrefix = "Inference_";
+    string inputFileName = "exponential.txt"; // data
+    string outputPathPrefix = "Exponential_";
 
     ifstream inputFile;
     File::openInputFile(inputFile, inputFileName);
@@ -111,12 +110,12 @@ int main(int argc, char *argv[])
     // Uniform Prior
     unsigned long Nparameters;  // Number of parameters for which prior distributions are defined
 
-    int Ndimensions = 2;        // Number of free parameters (dimensions) of the problem
+    int Ndimensions = 4;        // Number of free parameters (dimensions) of the problem
     vector<Prior*> ptrPriors(1);
     ArrayXd parametersMinima(Ndimensions);
     ArrayXd parametersMaxima(Ndimensions);
-    parametersMinima <<  0.5, 2.0;         // Minima values for the free parameters
-    parametersMaxima << 3.0, 20.0;     // Maxima values for the free parameters
+    parametersMinima <<  5, 0, 0, 0;         // Minima values for the free parameters
+    parametersMaxima << 15, 8, 6, 1;     // Maxima values for the free parameters
 
     UniformPrior uniformPrior(parametersMinima, parametersMaxima);
     ptrPriors[0] = &uniformPrior;
@@ -125,7 +124,7 @@ int main(int argc, char *argv[])
     uniformPrior.writeHyperParametersToFile(fullPathHyperParameters);
 
     // Set up the models for the inference problem
-    LinearModel model(covariates);      // Linear function of the type f = a*x + b
+    ParabolicModel model(covariates);      // Parabolic function
 
 
     // Set up the likelihood function to be used
@@ -133,29 +132,8 @@ int main(int argc, char *argv[])
 
     // Set up the K-means clusterer using an Euclidean metric
 
-    inputFileName = "Xmeans_configuringParameters.txt";
-    File::openInputFile(inputFile, inputFileName);
-    File::sniffFile(inputFile, Nparameters, Ncols);
-
-    if (Nparameters != 2)
-    {
-        cerr << "Wrong number of input parameters for X-means algorithm." << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    ArrayXd configuringParameters;
-    configuringParameters = File::arrayXXdFromFile(inputFile, Nparameters, Ncols);
-    inputFile.close();
-
-    int minNclusters = configuringParameters(0);
-    int maxNclusters = configuringParameters(1);
-
-    if ((minNclusters <= 0) || (maxNclusters <= 0) || (maxNclusters < minNclusters))
-    {
-        cerr << "Minimum or maximum number of clusters cannot be <= 0, and " << endl;
-        cerr << "minimum number of clusters cannot be larger than maximum number of clusters." << endl;
-        exit(EXIT_FAILURE);
-    }
+    int minNclusters = 1;  // Minimum number of clusters
+    int maxNclusters = 10; // Maximum number of clusters
 
     int Ntrials = 10;
     double relTolerance = 0.01;
@@ -165,40 +143,21 @@ int main(int argc, char *argv[])
 
     // Configure and start nested sampling inference -----
 
-    inputFileName = "NSMC_configuringParameters.txt";
-    File::openInputFile(inputFile, inputFileName);
-    File::sniffFile(inputFile, Nparameters, Ncols);
-    configuringParameters.setZero();
-    configuringParameters = File::arrayXXdFromFile(inputFile, Nparameters, Ncols);
-    inputFile.close();
-
-    if (Nparameters != 8)
-    {
-        cerr << "Wrong number of input parameters for NSMC algorithm." << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    bool printOnTheScreen = true;                       // Print results on the screen
-    int initialNobjects = configuringParameters(0);     // Initial number of live points
-    int minNobjects = configuringParameters(1);         // Minimum number of live points
-    int maxNdrawAttempts = configuringParameters(2);    // Maximum number of attempts when trying to draw a new sampling point
-    int NinitialIterationsWithoutClustering = configuringParameters(3); // The first N iterations, we assume that there is only 1 cluster
-    int NiterationsWithSameClustering = configuringParameters(4);       // Clustering is only happening every N iterations.
-    double initialEnlargementFraction = configuringParameters(5);   // Fraction by which each axis in an ellipsoid has to be enlarged.
-                                                                    // It can be a number >= 0, where 0 means no enlargement.
-    double shrinkingRate = configuringParameters(6);        // Exponent for remaining prior mass in ellipsoid enlargement fraction.
+    bool printOnTheScreen = true;                  // Print results on the screen
+    int initialNobjects = 1000;                     // Initial number of live points
+    int minNobjects = 1000;                         // Minimum number of live points
+    int maxNdrawAttempts = 10000;                  // Maximum number of attempts when trying to draw a new sampling point
+    int NinitialIterationsWithoutClustering = 500; // The first N iterations, we assume that there is only 1 cluster
+    int NiterationsWithSameClustering = 50;        // Clustering is only happening every N iterations.
+    double initialEnlargementFraction = 0.45;      // Fraction by which each axis in an ellipsoid has to be enlarged.
+                                                            // It can be a number >= 0, where 0 means no enlargement.
+    double shrinkingRate = 0.02;                   // Exponent for remaining prior mass in ellipsoid enlargement fraction.
                                                             // It is a number between 0 and 1. The smaller the slower the shrinkage
                                                             // of the ellipsoids.
 
-    if ((shrinkingRate > 1) || (shrinkingRate) < 0)
-    {
-        cerr << "Shrinking Rate for ellipsoids must be in the range [0, 1]. " << endl;
-        exit(EXIT_FAILURE);
-    }
+    double terminationFactor = 0.01;    // Termination factor for nested sampling process.
 
-    double terminationFactor = configuringParameters(7);    // Termination factor for nested sampling process.
-
-
+    // RUN NESTED SAMPLER
     MultiEllipsoidSampler nestedSampler(printOnTheScreen, ptrPriors, likelihood, myMetric, kmeans,
                                         initialNobjects, minNobjects, initialEnlargementFraction, shrinkingRate);
 
@@ -233,7 +192,7 @@ int main(int argc, char *argv[])
     bool writeMarginalDistributionToFile = true;
     results.writeParametersSummaryToFile("parameterSummary.txt", credibleLevel, writeMarginalDistributionToFile);
 
-    cout << "Process completed." << endl;
+    cout << "miniNS done." << endl;
 
     return EXIT_SUCCESS;
 }
